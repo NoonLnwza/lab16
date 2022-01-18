2c47d6d1d86e27458f96e2ba52389cad2412f936
Pubet Choompusueb <pubet50@gmail.com>
2022-01-18 11:18:18 +0700

    completed

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,29 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int w[4]={a,b,c,d};
+    int M[4];
+    int n=0,o=0,p=0,q=0;
+
+    n= rand()%4;
+    M[0]=w[n];
+    w[n]=0;
+    do{
+        o= rand()%4;
+    }while(w[o]==0);
+    M[1]=w[o];
+    w[o]=0;
+    do{
+        p= rand()%4;
+    }while(w[p]==0);
+    M[2]=w[p];
+    w[p]=0;
+    do{
+        q= rand()%4;
+    }while(w[q]==0);
+    M[3]=w[q];
+
+    a=M[0],b=M[1],c=M[2],d=M[3];
+}
